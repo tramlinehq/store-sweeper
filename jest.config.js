919d3e0789ce8e13594ba248fdddd0581f27b5f9
Sameer Kolhar <kolhar730@gmail.com>
2025-02-19 20:31:53 -0800

    update jest config and package.json

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,25 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
+  preset: "ts-jest",
   testEnvironment: "node",
   transform: {
     "^.+.tsx?$": ["ts-jest", {}],
+  },
+  testMatch: ["**/__tests__/**/*.test.ts"],
+  moduleFileExtensions: ["ts", "js"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  transformIgnorePatterns: [
+    "node_modules/(?!(google-play-scraper|ramda)/)",
+    "/vendor/(?!(google-play-scraper)/)",
+  ],
+  transform: {
+    "^.+\\.(ts|js)$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
   },
 };
